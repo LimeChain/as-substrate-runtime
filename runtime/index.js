5acddce75afa86176a02be205c63fa09fba24aa1
Daniel Ivanov <daniel@limechain.tech>
2020-07-20 11:20:18 +0300

    revert changes

--- runtime/index.js
+++ runtime/index.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 const loader = require("@assemblyscript/loader");
-const imports = {
-    ext_storage_get: function () {
-
-    }
-};
+const imports = { /* imports go here */ };
 const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/untouched.wasm"), imports);
 console.log(wasmModule.exports);
