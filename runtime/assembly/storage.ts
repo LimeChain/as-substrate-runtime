c5372768535ae17b6c2aa637009185fdf5f44a1f
Daniel Ivanov <daniel@limechain.tech>
2020-07-20 11:17:29 +0300

    add all host functions

--- runtime/assembly/storage.ts
+++ runtime/assembly/storage.ts
@@ -1,7 +1,6 @@
 import {Utils} from './utils';
 import { Serialiser } from './api/serialiser';
-import { ext_storage_set, ext_storage_get } from './env';
-import { Bytes } from 'as-scale-codec';
+import { ext_storage_set_version_1, ext_storage_get_version_1 } from './env';
 
 /**
  * Namespace exporting Storage related functions
@@ -15,7 +14,7 @@ export namespace Storage {
     export function get(key: string): u8[] {
         const u8Key = Uint8Array.wrap(String.UTF8.encode(key));
         const key64: u64 = Serialiser.serialise_result(Utils.toU8Array(u8Key));
-        const result: u64 = ext_storage_get(key64);
+        const result: u64 = ext_storage_get_version_1(key64);
         const ptr_size: i32[] = Serialiser.separate_pointer_size(result);
         return Serialiser.deserialise_input(ptr_size[0], ptr_size[1]);
     }
@@ -29,7 +28,7 @@ export namespace Storage {
         const u8Value = Uint8Array.wrap(String.UTF8.encode(value));
         const key64: u64 = Serialiser.serialise_result(Utils.toU8Array(u8Key));
         const value64: u64 = Serialiser.serialise_result(Utils.toU8Array(u8Value));
-        ext_storage_set(key64, value64);
+        ext_storage_set_version_1(key64, value64);
     }
 
 }
