0810c03a92fe411b05f0f16f33f6749fa840e961
Daniel Ivanov <daniel@limechain.tech>
2020-07-21 14:21:32 +0300

    change the compile sh to normal

--- sandbox/compile.sh
+++ sandbox/compile.sh
@@ -1,1 +1,1 @@
-cd ../runtime && npm run asbuild && cd ../sandbox && cargo test test_get_account_data -- --nocapture  && cd ..
+cd ../runtime && npm run asbuild && cd ../sandbox && cargo test && cd ..
