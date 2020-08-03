2ffef1f37813a1d3b50caee1879f10fcd7617f76
Dastan <dastanbeksamatov@gmail.com>
2020-08-03 16:53:03 +0300

    Add InherentProvider implementation for Timestamp

--- runtime/packages/modules/timestamp/assembly/timestamp.ts
+++ runtime/packages/modules/timestamp/assembly/timestamp.ts
@@ -1,5 +1,7 @@
 import { Storage } from '@as-substrate/core-modules';
-import { UInt64, Bool, ByteArray, ScaleString } from 'as-scale-codec';
+import { InherentData } from '@as-substrate/models';
+import { UInt64, Bool, ByteArray } from 'as-scale-codec';
+
 
 export class Timestamp{
 
@@ -9,7 +11,7 @@ export class Timestamp{
     public static readonly MINIMUM_PERIOD: u64 = 5;
     public static readonly SCALE_TIMESTAMP_NOW: u8[] = [36, 116, 105, 109, 101, 115, 116, 97, 109, 112, 12, 110, 111, 119];
     public static readonly SCALE_TIMESTAMP_DID_UPDATE: u8[] = [36, 116, 105, 109, 101, 115, 116, 97, 109, 112, 36, 100, 105, 100, 117, 112, 100, 97, 116, 101];
-
+    public static readonly INHERENT_IDENTIFIER: string = "timstmp0";
     /**
      * Sets the current time. When setting the new time, 
      * it must be greater than the last one (set into storage) with at least a MinimumPeriod
@@ -31,6 +33,7 @@ export class Timestamp{
         Storage.set(Timestamp.SCALE_TIMESTAMP_DID_UPDATE, trueu8.toU8a());
         Storage.set(Timestamp.SCALE_TIMESTAMP_NOW, nowu8.toU8a());
     }
+
     /**
      *  Gets the current time that was set. If this function is called prior 
      *  to setting the timestamp, it will return the timestamp of the previous block.
@@ -41,4 +44,42 @@ export class Timestamp{
         const val: UInt64 = UInt64.fromU8a(res);
         return val.value;
     }
+
+    /**
+     * Creates timestamp inherent data
+     * @param data inherent data to extract timestamp from
+     */
+    static createInherent(data: InherentData): void {
+        const timestampData: u64 = extractInherentData(data);
+        const nextTime = Math.max(timestampData, Timestamp.get() + Timestamp.MINIMUM_PERIOD);
+        Timestamp.set(nextTime);
+    }
+
+    /**
+     * Checks if the new value can be set as inherent data
+     * @param t new value of the timestamp inherent data
+     * @param data inherent data to extract timestamp from
+     */
+    static checkInherent(t: u64, data: InherentData): bool {
+        const MAX_TIMESTAMP_DRIFT_MILLS: u64 = 30 * 1000;
+        const timestampData: u64 = extractInherentData(data);
+        const minimum: u64 = Timestamp.get() + Timestamp.MINIMUM_PERIOD;
+        if (t > timestampData + MAX_TIMESTAMP_DRIFT_MILLS){
+            return false;
+        }
+        else if(t < minimum){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+/**
+ * Gets timestamp inherent data
+ * @param inhData inherentData instance provided 
+ */
+export function extractInherentData(inhData: InherentData): u64 {
+    return inhData.data[Timestamp.INHERENT_IDENTIFIER];
 }
