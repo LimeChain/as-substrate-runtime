1cc886fc4e2c86a10b5dfa0e46ebe151d4d96d04
Dastan <dastanbeksamatov@gmail.com>
2020-09-25 15:08:44 +0300

    Add extrinsicsRoot method

--- runtime/packages/core-modules/assembly/system.ts
+++ runtime/packages/core-modules/assembly/system.ts
@@ -1,9 +1,9 @@
 import { CompactInt, ByteArray, Bytes, UInt64, UInt32, Hash } from 'as-scale-codec';
 import { Storage } from './storage';
 import { Blocks, Header, ExtrinsicData } from '@as-substrate/models';
-import { Utils } from '@as-substrate/core-utils';
+import { Utils, Serialiser } from '@as-substrate/core-utils';
 import { AccountId } from '@as-substrate/balances-module';
-import { Crypto } from '.';
+import { ext_trie_blake2_256_ordered_root_version_1 } from '.';
 
 export class System {
     // execution phases
@@ -109,7 +109,7 @@ export class System {
     }
 
     /**
-     * Increment extrinsic count
+     * Increment extrinsicCount
      */
     static incExtrinsicCount(): void {
         const count = this.extrinsicCount();
@@ -122,9 +122,22 @@ export class System {
      * @param data 
      */
     static extrinsicsRoot(data: ExtrinsicData): Hash{
-        return Crypto.computeExtrinsicRoot(data.toOrderedTrie());
+        return this.computeExtrinsicRoot(data.toEnumeratedValues());
     }
 
+        /**
+     * Computes the orderedTrieRoot hash of the extrinsics data using 
+     * @param data enumerated values
+     */
+    static computeExtrinsicRoot(data: u8[]): Hash{
+        let dataPtr = data.dataStart;
+        let dataLen = data.length;
+        __retain(dataPtr);
+        const dataU64 = ((dataLen as u64) << 32) | dataPtr;
+        let extrinsicRoot: i32 = ext_trie_blake2_256_ordered_root_version_1(dataU64);
+        const hashU8a = Serialiser.deserialiseInput(extrinsicRoot, 32);
+        return Hash.fromU8a(hashU8a);
+    }
     /**
      * Adds applied extrinsic to the current ExtrinsicData
      * @param ext extrinsic as bytes
--- runtime/packages/models/assembly/extrinsic-data.ts
+++ runtime/packages/models/assembly/extrinsic-data.ts
@@ -14,7 +14,7 @@ export class ExtrinsicData{
     toU8a(): u8[]{
         let result: u8[] = [];
         let keys: UInt32[] = this.data.keys();
-        let lenData: CompactInt = new CompactInt(keys.length);
+        let lenData: CompactInt = new CompactInt(<u8>(keys.length));
         result = result.concat(lenData.toU8a());
         for(let i = 0; i < keys.length; i++){
             result = result
@@ -27,14 +27,17 @@ export class ExtrinsicData{
     /**
      * Enumerated items, from which orderedTrieRoot will get 
      * The items consist of a SCALE encoded array containing only values, corresponding 
-     * key of each value is the index of the item in the array, starting at 0. 
-     * The keys are little-endian, fixed-size integers.
+     * key of each value is the index of the item in the array, starting at 0.
+     * In our case same as toU8a() but with only values concatenated
      */
-    toOrderedTrie(): u8[][]{
-        let result: u8[][] = [];
-        let values: ByteArray[] = this.data.values();
-        for(let i=0; i < values.length; i++){
-            result.push(values[i].values);
+    toEnumeratedValues(): u8[]{
+        let result: u8[] = [];
+        let keys: UInt32[] = this.data.keys();
+        let lenData: CompactInt = new CompactInt(<u8>(keys.length));
+        result = result.concat(lenData.toU8a());
+        for(let i=0; i < keys.length; i++){
+            result = result
+            .concat(this.data.get(keys[i]).toU8a());
         }
         return result;
     }
